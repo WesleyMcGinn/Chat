c3b54b1857146190013a120bc05760763ff644a4
Wesley McGinn <wesleymcginn1@gmail.com>
2022-09-06 19:29:17 -0400

    Created Server.js
    Websocket is on port 8081.
    Page is on port 80 for easy access.

--- chat_server.js
+++ chat_server.js
@@ -1,1 +1,67 @@
+const fs = require('fs');
+const http = require('http');
+const ws = require('ws');
+const ws_server = new ws.Server({noServer: true});
+var clients = new Set();
+var people = 0;
 
+const saveInterval = 5;
+
+var chatData;
+fs.readFile("C:/Users/admin/Desktop/GitHub Stuff/Chat/chatData.dat", function (err, data) {
+    if (err) {
+        console.log("Error:  The file for chat room storage was not found.")
+    } else {
+        chatData = data;
+    }
+});
+
+http.createServer((req, res) => {
+    ws_server.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
+}).listen(8081);
+
+http.createServer((req, res) => {
+    if (req.url == '' || req.url == '/' || req.url == '/chat') {
+        directory = "C:/Users/admin/Desktop/Github Stuff/Chat/index.html";
+    } else {
+        directory = "C:/Users/admin/Desktop/Github Stuff/Chat" + req.url;
+    }
+    fs.readFile("C:/Users/admin/Desktop/Github Stuff/Chat/index.html", function (err, data) {
+        if (err) {
+            console.log("404 Error for: " + req.url);
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.write("<h1>404</h1><h2>The file you have requested does not exist.</h2><h1>:(</h1>");
+            return res.end();
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+        }
+    });
+}).listen(80);
+
+function onSocketConnect(ws) {
+    clients.add(ws);
+    people++;
+    console.log(people);
+    ws.on('message', function (message) {
+        chatData = "" + message;
+        for (let client of clients) {
+            client.send(chatData);
+        }
+    });
+    ws.on('close', function() {
+        clients.delete(ws);
+        people--;
+        console.log(people);
+    });
+}
+
+function saveData() {
+    fs.writeFile('C:/Users/admin/Desktop/GitHub Stuff/Chat/chatData.dat', chatData, function(err, file) {
+        if (err) throw err;
+        console.log("New data saved to file.");
+    });
+}
+
+window.setInterval("saveData()", 60000 * saveInterval);
