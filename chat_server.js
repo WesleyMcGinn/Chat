c8e2ffa902f3f7a82e3504d2a1e8789fe23cd8ad
Wesley McGinn <wesleymcginn1@gmail.com>
2022-09-17 20:28:12 -0400

    Programmed Server
    The server should now behave and store data as necessary.  Now that a server and websockets must be involved in the programming, the whole system for data management has changed.

--- chat_server.js
+++ chat_server.js
@@ -1,38 +1,43 @@
+const chatDataDirectory = "C:/Users/admin/Desktop/Server/chatData0.dat";
+const backupDataDirectory = "C:/Users/admin/Desktop/Server/backup";
+
 const fs = require('fs');
 const http = require('http');
 const ws = require('ws');
 const ws_server = new ws.Server({noServer: true});
 var clients = new Set();
 var people = 0;
-
-var chatDataDirectory = "C:/Users/admin/Desktop/Server/chatData0.dat";
 var chatData;
+const defaultChatData = [['TESTROOM', 'PASSWORD', [], ["<div style='padding: 7px; background-color: #072dc780; border-radius: 12px'>Welcome to the testroom!  This is where programming for Chat is tested.</div>"]]];
+
+// Retrieve chat data from file:
 fs.readFile(chatDataDirectory, function (err, data) {
     if (err) {
         console.log("Error:  The file for chat room storage was not found.");
         console.log("        It will be created when the websocket recieves input.");
-        chatData = "";
+        chatData = defaultChatData;
     } else {
         chatData = data;
     }
 });
 
-http.createServer((req, res) => {
-    ws_server.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
-}).listen(8081);
+var adminPassword;fs.readFile("C:/Users/admin/adminData/pw.dat",function(err,data){if(err){console.log("Error in locating admin key");}else{adminPassword=data;}});
 
+// HTML Server:
 http.createServer((req, res) => {
-    if (req.url == '' || req.url == '/' || req.url == '/chat') {
-        directory = "C:/Users/admin/Desktop/Github Stuff/Chat/index.html";
-    } else {
-        directory = "C:/Users/admin/Desktop/Github Stuff/Chat" + req.url;
-    }
-    fs.readFile(directory, function (err, data) {
-        if (err) {
-            console.log("404 Error for: " + req.url);
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            res.write("<h1>404</h1><h2>The file you have requested does not exist.</h2><h1>:(</h1>");
-            return res.end();
+    fs.readFile("C:/Users/admin/Desktop/Github Stuff/Chat/" + req.url, function (err, data) {
+        if (err || req.url == '') {
+            fs.readFile("C:/Users/admin/Desktop/Github Stuff/Chat/index.html", function (err2, data2) {
+                if (err2) {
+                    console.log("ERROR 404: index.html not found!");
+                    res.writeHead(404);
+                    res.write("<h1>404</h1><h2>Page not found.    ): </h2>");
+                } else {
+                    res.writeHead(200);
+                    res.write(data2);
+                }
+            });
+            res.end();
         } else {
             res.writeHead(200);
             res.write(data);
@@ -41,26 +46,134 @@ http.createServer((req, res) => {
     });
 }).listen(80);
 
-function onSocketConnect(ws) {
+// Websocket Server:
+http.createServer((req, res) => {
+    ws_server.handleUpgrade(req, req.socket, Buffer.alloc(0), websocketHandler);
+}).listen(7537);
+
+// Websocket Programming:
+function websocketHandler(ws) {
+
     clients.add(ws);
     people++;
-    console.log(people);
-    for (let client of clients) {
-        client.send(chatData);
-    }
+    console.log("Current Users + : " + people.toString());
+
     ws.on('message', function (message) {
-        chatData = "" + message;
-        for (let client of clients) {
-            client.send(chatData);
+        var incomingData = "" + message;
+        var dataType = incomingData.substring(0, 1);
+        var dataData = incomingData.substring(1, incomingData.length);
+        if (dataType == "G") { // Get Message List
+            ws.send("G" + JSON.stringify(chatData[parseInt(dataData)]));
         }
-        fs.writeFile(chatDataDirectory, chatData, function(err, file) {
-            if (err) throw err;
-            console.log("New data saved to file.");
-        });
+        if (dataType == "V") { // Validate Room Name and Password
+            var validated = "N";
+            for (i = 0; i < chatData.length; i++) {
+                if (chatData[i][0] + "--u:p--" + chatData[i][1] == dataData) {
+                    validated = i;
+                    break;
+                }
+            }
+            ws.send("V" + validated);
+        }
+        if (dataType == "S") { // Test Network Speed
+            ws.send("S" + new Date().getTime().toString());
+        }
+        if (dataType == "N") { // New Chat Room
+            dataData = eval(dataData);
+            ws.send("N" + (chatData.push([dataData[0], dataData[1], [], []])).toString());
+            saveData();
+        }
+        if (dataType == "L") { // Leave Chat Room
+            chatData.splice(chatData[parseInt(dataData)][2].indexOf(ws), 1)[0].send("L");
+        }
+        if (dataType == "C") { // Clear Chat Room
+            chatData[parseInt(dataData)][3] = [];
+            sendToRoom(parseInt(dataData), "Cleared");
+            saveData();
+        }
+        if (dataType == "M") { // Send Message
+            dataData = eval(dataData);
+            chatData[parseInt(dataData[0])][3].unshift(dataData[1]);
+            sendToRoom(parseInt(dataData)[0], "M" + dataData[1]);
+            saveData();
+        }
+        if (dataType == "O") {
+            dataData = eval(dataData);
+            var myColor = generateColor();
+            for (i = 0; i < chatData[dataData[0]][3].length; i++) {
+                if (chatData[dataData[0]][3][i].includes(dataData[1])) {
+                    chatData[dataData[0]][3][i] = chatData[dataData[0]][3][i].replace(dataData[1], myColor);
+                }
+            }
+            sendToRoom(parseInt(dataData)[0], "O" + JSON.stringify(chatData[parseInt(dataData[0])]));
+            saveData();
+        }
+        if (dataType == ".") {
+            if (dataData != adminPassword) {
+                ws.send(".Error!  Your password was incorrect and you have been labled as a threat!");
+                console.log("HACKER DETECTED!");
+                console.log("  Time: " + (new Date()).toString())
+                console.log("  Incorrect Password Used: " + datadata);
+                console.log("  Attempted to: Obtain all data");
+                console.log("  Danger Level: 1 (Low)");
+            } else {
+                ws.send("." + JSON.stringify(chatData));
+            }
+        }
+        if (dataType == ",") {
+            if (dataData != adminPassword) {
+                ws.send(".Error!  Your password was incorrect and you have been labled as a threat!");
+                console.log("HACKER DETECTED!");
+                console.log("  Time: " + (new Date()).toString())
+                console.log("  Incorrect Password Used: " + datadata);
+                console.log("  Attempted to: Clear all data");
+                console.log("  Danger Level: 2 (Considerably Dangerous)");
+            } else {
+                var roomsToBeErased = chatData.length - 1;
+                saveData(true);
+                chatData = defaultChatData;
+                saveData();
+                console.log("ALL CHAT DATA CLEARED:");
+                console.log("  Time: " + (new Date()).toString())
+                console.log("  Rooms Deleted: " + roomsToBeErased);
+                console.log("  Backup Saved: true");
+            }
+        }
     });
+
     ws.on('close', function() {
         clients.delete(ws);
         people--;
-        console.log(people);
+        console.log("Current Users - : " + people);
     });
+
+}
+
+function saveData(backup = false) {
+    var chatData2 = chatData;
+    for (i = 0; i < chatData2.length; i++) {
+        chatData2[i][2] = [];
+    }
+    fs.writeFile(chatDataDirectory, chatData2, function(err, file) {
+        if (err) { throw err; }
+    });
+    if (backup == true) {
+        fs.writeFile(backupDataDirectory + generateColor() + ".dat", chatData2, function(err, file) {
+            if (err) { throw err; }
+        });
+    }
+}
+
+function sendToRoom(room_number, message_to_room) {
+    for (i = 0; i < chatData[room_number][2].length; i++) {
+        chatData[room_number][2][i].send(message_to_room);
+    }
+}
+
+function generateColor() {
+    var output = '';
+    for (i = 0; i < 6; i++) {
+        output += (Math.floor(Math.random()*16).toString(16));
+    }
+    return "#" + output + "80";
 }
