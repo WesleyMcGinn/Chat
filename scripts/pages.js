2ba43d3a7fc38acb07dd2c1d800734d9974263f9
Wesley McGinn <wesleymcginn1@gmail.com>
2022-09-19 16:57:34 -0400

    Renamed Pages and Programmed Page Changer

---
+++ scripts/pages.js
@@ -0,0 +1,22 @@
+// This file contains the code for changing pages and interacting with chat rooms
+
+function page(page_number) {
+    if (page_number == 1 || page_number == "home") {
+        document.getElementById("Heading").innerHTML = "Chat";
+        document.getElementById("Home").style.display = '';
+        document.getElementById("New").style.display = 'none';
+        document.getElementById("Chat").style.display = 'none';
+    }
+    if (page_number == 2 || page_number == "new") {
+        document.getElementById("Heading").innerHTML = "Create New Chat Room";
+        document.getElementById("Home").style.display = 'none';
+        document.getElementById("New").style.display = '';
+        document.getElementById("Chat").style.display = 'none';
+    }
+    if (page_number == 3 || page_number == "chat") {
+        document.getElementById("Heading").innerHTML = "Chat Room: <i>" + document.getElementById("Room Input").value + "</i>";
+        document.getElementById("Home").style.display = 'none';
+        document.getElementById("New").style.display = 'none';
+        document.getElementById("Chat").style.display = '';
+    }
+}
